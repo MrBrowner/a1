e1f3334caddf5ebb5269801edd748283437d0301
browne_as <24374030+MrBrowner@users.noreply.github.com>
2026-02-13 16:46:32 +0530

    ... ** android declare not req here

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -46,34 +46,4 @@ kotlin {
     }
 }
 
-android {
-    namespace = "in.das.a1"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    defaultConfig {
-        applicationId = "in.das.a1"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-dependencies {
-    debugImplementation(libs.compose.uiTooling)
-}
 
