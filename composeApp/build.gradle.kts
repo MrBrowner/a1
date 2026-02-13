1d2c117e7de2f7a3b71609d9e1baf631f194937a
browne_as <24374030+MrBrowner@users.noreply.github.com>
2026-02-13 16:46:44 +0530

    ... ** use of androidKmpLibrary

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -3,18 +3,19 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.androidKmpLibrary)
 }
 
 kotlin {
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
+    androidLibrary {
+        namespace = "in.das.androidapp"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+//        experimentalProperties["android.experimental.kmp.enableAndroidResources"] = true
     }
-    
+
     listOf(
         iosArm64(),
         iosSimulatorArm64()
